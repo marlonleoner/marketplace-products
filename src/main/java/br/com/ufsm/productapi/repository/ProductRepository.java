25b0a384a471d7f66c48d17832f08289b55939bf
Marlon Leoner <marlon.leoner@gmail.com>
2021-02-09 14:32:35 -0300

    criacao de buscas

--- src/main/java/br/com/ufsm/productapi/repository/ProductRepository.java
+++ src/main/java/br/com/ufsm/productapi/repository/ProductRepository.java
@@ -1,14 +1,25 @@
 package br.com.ufsm.productapi.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ufsm.productapi.model.Product;
 
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Optional<Product> findByName(String name);
+
+	Optional<Product> findByIdAndAmountGreaterThanAndAmountLessThan(Long id, Integer minAmount, Integer maxAmount);
+
+	List<Product> findByNameContains(String name);
+
+	List<Product> findByPriceGreaterThanAndPriceLessThan(Double min, Double max);
+
+	List<Product> findByNameContainsAndPriceGreaterThanAndPriceLessThan(String name, Double min, Double max);
+
+	List<Product> findByIdIn(List<Long> ids);
+	
 	
 }
