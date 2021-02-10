512e26d26ebdbdc2761a39f2e79c0883633e4f45
Marlon Leoner <marlon.leoner@gmail.com>
2021-02-10 14:59:42 -0300

    alteracao

--- src/main/resources/db/migration/V003__insert_products.sql
+++ src/main/resources/db/migration/V003__insert_products.sql
@@ -1,8 +1,8 @@
 INSERT INTO product
 	(id, name, price, type, amount, description, created_at, updated_at)
 VALUES
-	(1, "Jaqueta Corta Vento", 249.99, "ROUPAS", 7, "Jaqueta Corta Vento Chess Clothing Preto", GETDATE(), GETDATE()),
-	(2, "Tênis Confort Flex Infantil", 89.90, "CALÇADOS", 4, "Tênis Confort Flex Infantil confeccionado em material Neoprene", GETDATE(), GETDATE()),
-	(3, "Relógio", 149.90, "ACESSÓRIOS", 11, "Relógio Magnético Feminino Com Brilho Rosa", GETDATE(), GETDATE()),
-	(4, "Short Nike", 119.99, "ESPORTE", 23, "Short Nike Sportswear Nsw Blub Jsy Preta", GETDATE(), GETDATE()),
-	(5, "Moletom Camuflado", 79.00, "ROUPAS", 16, "Moletom Camuflado Canguru Lucas Lunny", GETDATE(), GETDATE());
+	(10001, "Jaqueta Corta Vento", 249.99, "ROUPAS", 7, "Jaqueta Corta Vento Chess Clothing Preto", NOW(), NOW()),
+	(10002, "Tênis Confort Flex Infantil", 89.90, "CALÇADOS", 4, "Tênis Confort Flex Infantil confeccionado em material Neoprene", NOW(), NOW()),
+	(10003, "Relógio", 149.90, "ACESSÓRIOS", 11, "Relógio Magnético Feminino Com Brilho Rosa", NOW(), NOW()),
+	(10004, "Short Nike", 119.99, "ESPORTE", 23, "Short Nike Sportswear Nsw Blub Jsy Preta", NOW(), NOW()),
+	(10005, "Moletom Camuflado", 79.00, "ROUPAS", 16, "Moletom Camuflado Canguru Lucas Lunny", NOW(), NOW());
